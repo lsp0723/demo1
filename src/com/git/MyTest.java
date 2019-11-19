e41e006163147b910cf35ffa12780cb943c22dee
lsp0723 <liu_shengpeng@163.com>
2019-11-19 17:12:26 +0800

    Mytest commit by clone

---
+++ src/com/git/MyTest.java
@@ -0,0 +1,13 @@
+package com.git;
+
+/**
+ * @author iask
+ * @date 2019/11/19
+ */
+public class MyTest {
+    public static void main(String[] args) {
+        for (int i = 0; i <10 ; i++) {
+            System.out.println("i am num="+i);
+        }
+    }
+}
